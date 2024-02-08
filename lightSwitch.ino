ff5b7af9a6accc8f249c48e874c67c2783ad061f
Robert Snell <91638194+roberts1y@users.noreply.github.com>
2024-02-08 12:04:13 -0500

    update file with current work

--- lightSwitch.ino
+++ lightSwitch.ino
@@ -1,33 +1,62 @@
-int ledPin = 13;  // LED connected to digital pin 13
-int pirPin = 2;   // PIR sensor connected to pin 2
-int pirState = LOW;  // Initializing the PIR state
-unsigned long lastMotionTime = 0;  // Time when the last motion was detected
+#include <Servo.h>
+
+const int pirPin = 7; // PIR on D7
+const int buttonPin = 6; // Button on D6
+const int servoPin = 12; // Servo Motor on D12
+
+Servo myservo; 
+
+int pirState = LOW; 
+int buttonState = LOW; // Current state of the button
+int lastButtonState = LOW; // Previous state of the button
+bool toggleState = false; // Toggle state for servo position
+
+// Servo positions
+int upPosition = 180; // Adjust to  servo's up position
+int downPosition = 0; // Adjust to servo's down position
+
+unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
+unsigned long debounceDelay = 50; // time for user to release button
 
 void setup() {
-  pinMode(ledPin, OUTPUT);  // Initialize the LED pin as an output
-  pinMode(pirPin, INPUT);   // Initialize the PIR sensor pin as an input
-  Serial.begin(9600);       // Start serial communication
+  pinMode(pirPin, INPUT);
+  pinMode(buttonPin, INPUT);
+  myservo.attach(servoPin);
+  myservo.write(downPosition);
 }
 
 void loop() {
-  int sensorValue = digitalRead(pirPin); // Read the value from the PIR sensor
+  int reading = digitalRead(buttonPin);
+  
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+  
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
 
-  if (sensorValue == HIGH) {
-    lastMotionTime = millis();  // Update the last motion time
-    if (pirState == LOW) {
-      // PIR state is changed to HIGH
-      Serial.println("Motion detected!");
-      pirState = HIGH;
-      digitalWrite(ledPin, HIGH);  // Turn the LED on
+      if (buttonState == HIGH) {
+        toggleState = !toggleState; // Toggle the state
+        if (toggleState) {
+          myservo.write(upPosition);
+        } else {
+          myservo.write(downPosition);
+        }
+      }
     }
-  } 
+  }
 
-  if (millis() - lastMotionTime > 1000) {  // Check if it has been 1 second since last motion
-    if (pirState == HIGH){
-      // PIR state is changed to LOW
-      Serial.println("Motion ended!");
-      pirState = LOW;
-      digitalWrite(ledPin, LOW);  // Turn the LED off
+  lastButtonState = reading;
+
+  // PIR sensor
+  pirState = digitalRead(pirPin);
+  if (pirState == HIGH) {
+    myservo.write(upPosition); // Move servo to up position
+    delay(5000); 
+    if (!toggleState) { // If not manually overridden
+      myservo.write(downPosition); // Move servo back to down position
     }
   }
 }
