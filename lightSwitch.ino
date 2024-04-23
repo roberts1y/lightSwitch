488c41dc32eb030699383ac63e92f5b1b6d49ade
Robert Snell <91638194+roberts1y@users.noreply.github.com>
2024-04-23 08:49:14 -0400

    feat: add fallback timer & turn off LED

--- lightSwitch.ino
+++ lightSwitch.ino
@@ -1,9 +1,9 @@
 #include <Servo.h>
 
-const int pirPin = 7; // PIR on D7
+const int pirPin = 9; // PIR on D7
 const int buttonPin = 4; // Button on D4
 const int servoPin = 5; // Servo Motor on D5
-const int LEDPin = 2; // LED on D2
+const int LEDPin = 1; // LED on D2
 
 Servo myservo;
 
@@ -21,13 +21,15 @@ unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
 unsigned long debounceDelay = 50; // time for user to release button
 
 unsigned long pirActivatedTimestamp = 0; // Timestamp of when the PIR sensor last activated the servo
-const long pirActivationDuration = 5000; // Duration to keep the servo in the up position after PIR activation
+const long pirActivationDuration = 9500; // Duration to keep the servo in the up position after PIR activation
 
 // New variables for PIR debounce
 unsigned long lastPirEvent = 0; // Last time a PIR event was recorded
-unsigned long pirDebounceDelay = 10000; // Delay to debounce PIR sensor
+unsigned long pirDebounceDelay = 4500; // Delay to debounce PIR sensor
 
 unsigned long currentMillis;
+unsigned long upTime;
+bool up = false;
 
 void setup() {
   Serial.begin(9600);
@@ -35,9 +37,11 @@ void setup() {
   pinMode(pirPin, INPUT);
   pinMode(buttonPin, INPUT);
   pinMode(LEDPin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 
   myservo.attach(servoPin);
   myservo.write(downPosition);
+  digitalWrite(LED_BUILTIN, LOW);
   digitalWrite(LEDPin, LOW); // Initially, the LED is off
 }
 
@@ -58,6 +62,7 @@ void loop() {
           // Override PIR activation
           myservo.write(downPosition);
           digitalWrite(LEDPin, LOW); // Turn LED off
+          up = false;
           isPirActive = false; // Reset PIR activation flag
         } else {
           // Toggle servo position normally
@@ -65,14 +70,24 @@ void loop() {
           if (toggleState) {
             myservo.write(upPosition);
             digitalWrite(LEDPin, HIGH); // Turn LED on
+            up = true;
+            upTime = currentMillis;
           } else {
             myservo.write(downPosition);
             digitalWrite(LEDPin, LOW); // Turn LED off
+            up = false;
           }
         }
       }
     }
   }
+
+  // // Check if the switch is up and it's been 10 seconds
+  // if (up && (currentMillis - upTime >= 11000)) {    
+  //   myservo.write(downPosition); // Move the switch down
+  //   digitalWrite(LEDPin, LOW); // Turn LED off
+  //   up = false;
+  // }
 
   lastButtonState = reading;
 
@@ -80,8 +95,8 @@ void loop() {
   pirState = digitalRead(pirPin);
   currentMillis = millis();
 
-  // Serial.print("PIR");
-  // Serial.println(pirState);
+  Serial.print("PIR");
+  Serial.println(pirState);
 
   Serial.print("Current");
   Serial.println(currentMillis);
